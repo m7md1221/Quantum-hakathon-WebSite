5c48f59014324aa8182396f71049bb6b4e1a03b3
m7md1221 <derghamdawa@gmail.com>
2026-02-07 01:17:04 +0200

    Replace 'View Teams' button icon with SVG for improved aesthetics

--- public/admin.js
+++ public/admin.js
@@ -193,7 +193,11 @@ async function loadTeams() {
 
   <div class="hall-toggle-section">
     <span class="hall-toggle-text">View Teams</span>
-    <span class="hall-toggle-icon">▶</span>
+    <span class="hall-toggle-icon" style="display:inline-block;vertical-align:middle;">
+      <svg width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+        <path d="M9 18l6-6-6-6"/>
+      </svg>
+    </span>
   </div>
 </div>
 
